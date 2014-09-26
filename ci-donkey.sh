f4df7b80e1ba849850781f4d517b8a7a35295520
Samuel Colvin <s@muelcolvin.com>
2014-09-26 20:07:47 +0100

    update ci script

--- ci-donkey.sh
+++ ci-donkey.sh
@@ -4,6 +4,9 @@ virtualenv env
 echo "we haven't changed source so remember to manually call the correct python (or other script)"
 echo "this is where you would install requirements and setup the enviroment"
 
+echo "we have to copy the source into place since it's in a read only mounted volume"
+cp /src/* .
+
 # <MAIN SCRIPT>:
 # python manage.py test
 echo "now we're going to actually test the system"
