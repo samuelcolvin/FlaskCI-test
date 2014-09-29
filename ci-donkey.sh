e24756903e485bd9e576f05392f60b7c46c87cb2
Samuel Colvin <s@muelcolvin.com>
2014-09-29 17:42:35 +0100

    add splitter to ci

--- ci-donkey.sh
+++ ci-donkey.sh
@@ -6,8 +6,10 @@ echo "this is where you would install requirements and setup the enviroment"
 
 echo "we have to copy the source into place since it's in a read only mounted volume"
 cp /src/* .
-sleep 90
-# <MAIN SCRIPT>:
-# python manage.py test
+echo "before splitter"
+
+echo "**MAIN SCRIPT**"
+
+echo "after splitter"
 echo "now we're going to actually test the system"
 ./env/bin/python sample_test.py
