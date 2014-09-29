9db71e45153f22aa235bbc6001a95e2d7786d058
Samuel Colvin <s@muelcolvin.com>
2014-09-29 17:48:04 +0100

    add bumf to ci

--- ci-donkey.sh
+++ ci-donkey.sh
@@ -1,7 +1,7 @@
-# continuous integration script fror FlaskCI
-# <PRE SCRIPT>:
+# !/bin/bash
+set -e
 virtualenv env
-./env/bin/install django uWSGI
+./env/bin/pip install django uWSGI
 echo "we haven't changed source so remember to manually call the correct python (or other script)"
 echo "this is where you would install requirements and setup the enviroment"
 
